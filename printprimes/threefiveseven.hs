08df29dffe190fefb606fc71f6abc2ee6dffbb82
Dustin Horvath <dustinmhorvath@gmail.com>
2015-05-04 16:15:19 -0500

    Initial commit of threefiveseven.hs. Works correctly, prints n elements that are not divisible by 3 5 or 7.

---
+++ printprimes/threefiveseven.hs
@@ -0,0 +1,18 @@
+noFactors i = (i `mod` 3 /= 0 && i `mod` 5 /= 0 && i `mod` 7 /= 0)
+
+nextNone i n x = do
+ if(n < x)
+  then
+   if(noFactors i)
+    then
+     do
+      print i
+      nextNone (i+1) (n+1) x
+    else
+     nextNone (i+1) n x
+  else
+   return()
+
+enumerate x = do
+ nextNone 1 0 x
+ return()
