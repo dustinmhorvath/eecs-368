508d2447c608e723eac82d8d92deafbb98964a4d
Dustin Horvath <dustin@server.nichnologist.net>
2015-05-06 21:56:12 -0500

    Much improved (and accurate) sieve of Eratosthenes functions. Program is complete.

--- printprimes/printprimes.hs
+++ printprimes/printprimes.hs
@@ -1,3 +1,21 @@
+{-# OPTIONS_GHC -O2 #-}
+import Data.Array.Unboxed
+ 
+--minus block adapted from Pedro R. on Stackoverflow, used
+--because I wanted a prefix minus operator for listPrimes
+minus :: Ord a => [a] -> [a] -> [a]
+minus [] _ = []
+minus xs [] = xs
+minus l1@(x:xs) l2@(y:ys)
+    | x > y = minus l1 ys
+    | x < y = x : minus xs l2
+    | otherwise = minus xs l2
+
+--returns primes up to n using sieve of E
+sieveE n = 2 : sieve [3,5..n]  where
+    sieve (x:xs) 
+       | x*x > n   = x : xs
+       | otherwise = x : sieve (xs `minus` [x*x, x*x+2*x..])
 
 isPrime k = null [ x | x <- [2..k - 1], k `mod`x  == 0]
 
@@ -14,9 +32,13 @@ getSuf i n x = do
   else
    return()
 
+-- returns x number of primes using brute force
 getPrimes x = do
  getSuf 2 0 x
  return()
 
---main = do
--- print 5
+--returns x primes using sieve of E
+takePrimes x = take x (sieveE 1000000)
+
+--returns the x'th prime using sieve of E
+getNthPrime x = last (takePrimes x)
